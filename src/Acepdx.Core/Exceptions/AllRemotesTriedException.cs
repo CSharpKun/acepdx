838b916870e6741a59cba6bfc6d7be2742b41122
CSharpKun <CSharpKunDev@proton.me>
2026-08-25 19:31:09 +0300

    refactor: remove base exception class

--- src/Acepdx.Core/Exceptions/AllRemotesTriedException.cs
+++ src/Acepdx.Core/Exceptions/AllRemotesTriedException.cs
@@ -1,10 +1,14 @@
 namespace Acepdx.Core.Exceptions;
 
-public class AllRemotesTriedException : AcepdxException
+public class AllRemotesTriedException : Exception
 {
     public AllRemotesTriedException(string message, Exception ex)
         : base(message, ex) { }
 
     public AllRemotesTriedException(string message)
         : base(message) { }
+
+    public AllRemotesTriedException()
+    {
+    }
 }
--- src/Acepdx.Core/Exceptions/NoRemotesFoundException.cs
+++ src/Acepdx.Core/Exceptions/NoRemotesFoundException.cs
@@ -1,10 +1,14 @@
 namespace Acepdx.Core.Exceptions;
 
-public sealed class NoRemotesFoundException : AcepdxException
+public sealed class NoRemotesFoundException : Exception
 {
     public NoRemotesFoundException(string message, Exception ex)
         : base(message, ex) { }
 
     public NoRemotesFoundException(string message)
         : base(message) { }
+
+    public NoRemotesFoundException()
+    {
+    }
 }
